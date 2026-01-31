16b55024ba03d29f6801b309fd3f1ddc6e3bd6cd
Anxten <allanjeremyb@gmail.com>
2026-01-31 17:17:25 +0700

    Feat: Create stock ticker configuration for LQ45 Indonesia

---
+++ src/config.py
@@ -0,0 +1,21 @@
+# Daftar Saham yang akan dianalisis (LQ45 Bluechips)
+TICKERS = [
+    'BBCA.JK', # Bank Central Asia
+    'BBRI.JK', # Bank Rakyat Indonesia
+    'BMRI.JK', # Bank Mandiri
+    'BBNI.JK', # Bank Negara Indonesia
+    'TLKM.JK', # Telkom Indonesia
+    'ASII.JK', # Astra International
+    'UNVR.JK', # Unilever Indonesia
+    'ICBP.JK', # Indofood CBP
+    'GOTO.JK', # GoTo Gojek Tokopedia
+    'ADRO.JK', # Adaro Energy
+    'PTBA.JK', # Bukit Asam
+    'ANTM.JK', # Aneka Tambang
+    'KLBF.JK', # Kalbe Farma
+    'AMRT.JK', # Sumber Alfaria (Alfamart)
+    'PGAS.JK', # Perusahaan Gas Negara
+]
+
+START_DATE = '2023-01-01'
+END_DATE = '2025-01-01'
